c7d36dd4371f0e4ac6a79c32b64d45baafac32d7
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-03-07 22:12:06 -0800

    Complete Exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let result = "";
+    for (let i = 1; i = num; i++) {
+        result += string;
+    }
+    return string;
 };
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
