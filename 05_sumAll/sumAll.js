f6c04ae07730d541b5b740315c2faa7f8127829a
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-03-09 22:08:55 -0800

    Complete Remove and Sum All exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+    let min = first;
+    let max = last;
+    let total = 0;
 
+    if (typeof (first) !== "number" || typeof (last) !== "number") {
+        return "ERROR";
+    } else {
+        if (first < 0 || last < 0) {
+            return "ERROR"
+        } else {
+            if (first > last) {
+                let min = last;
+                let max = first;
+                for (let i = min; i < max + 1; i++) {
+                    total += i;
+                }
+                return total;
+            } else {
+                for (let i = min; i < max + 1; i++) {
+                    total += i;
+                }
+                return total;
+            }
+        }
+    }
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
