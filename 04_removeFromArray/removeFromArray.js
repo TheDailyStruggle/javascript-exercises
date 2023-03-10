018d4ad8a5163aa1fa754d5cb8777bbba5f4352a
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-03-09 22:09:30 -0800

    Update remaining exercise files

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...theArgs) {
+    const newArray = [];
+    array.forEach((arg) => {
+        if (!theArgs.includes(arg)) {
+            newArray.push(arg)
+        }
+    })
+    return newArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
