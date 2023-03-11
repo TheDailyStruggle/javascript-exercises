affc5bd50863f9b8970962d6ef236abdc583d8af
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-03-10 22:22:08 -0800

    Fix remaining tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (input) {
+    let year = input;
+    if ((year % 400) === 0) {
+        return true;
+    } else if ((year % 4) === 0 && (year % 100) !== 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
