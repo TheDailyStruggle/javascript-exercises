30b18362ca591245494c53869e0de08a1d280e2d
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-03-15 20:36:33 -0700

    Complete Temp Conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (imperial) {
+  celsius = ((imperial - 32) * (5 / 9))
+  roundedC = Math.round(celsius * 10) / 10;
+  return roundedC
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (metric) {
+  farenheit = ((metric * 9) / 5 + 32)
+  roundedF = Math.round(farenheit * 10) / 10;
+  return roundedF
 };
 
 // Do not edit below this line
