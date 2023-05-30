5da877e30950da2d61ca5786c965c4e9652d8e7c
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-05-30 12:36:08 -0700

    Complete get titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    // const theTitles = books.map((book) => book.title)
+    // return theTitles;
+    const theTitles = books.map(function (book) {
+        return book.title
+    })
+    return theTitles
 };
 
 // Do not edit below this line
