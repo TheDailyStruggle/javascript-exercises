bee82c9875496a5a25af2183f93decc56b0a67f6
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-05-30 11:41:53 -0700

    Complete palindromes exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let alphaOnly = string.replaceAll(/[^0-9a-z]/gi, "");
+    let noSpaces = alphaOnly.replace(" ", "");
+    let lowerCase = noSpaces.toLowerCase();
+    let splitString = lowerCase.split("");
+    let reverseArray = splitString.reverse();
+    let reversedString = reverseArray.join();
+    let newString = reversedString.replaceAll(",", "");
 
+    if (lowerCase === newString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
