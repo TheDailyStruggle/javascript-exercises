dea22852b952cb74bc0d3598fbc2e3598320e5b6
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-05-30 12:16:08 -0700

    Complete Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    if (num <= 0) {
+        return ('OOPS');
+    } else {
+        const number = parseInt(num);
+        let a = 0;
+        let b = 1;
+        for (let i = 2; i <= number; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
