246f96f1ddb83903fbd884efe1c24a1122f051d6
TheDailyStruggle <chadmontgomery46@gmail.com>
2023-06-05 22:03:44 -0700

    Resync

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  const sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  const total = a - b;
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  const total = array.reduce(function (p, c) {
+    return p + c;
+  }, 0)
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  const total = array.reduce(function (p, c) {
+    return p * c;
+  }, 1)
+  return total;
 };
 
-const power = function() {
-	
+const power = function (n, e) {
+  const total = n ** e;
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let total = 1
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    if (people.yearOfDeath !== Number) {
+        yearOfDeath = (new Date()).getFullYear();
+    };
+
+    const oldest = people.sort((firstP, secondP) => {
+        if (firstP.yearOfDeath < secondP.yearOfDeath) {
+            return -1;
+        }
+        if (firstP.yearOfDeath > secondP.yearOfDeath) {
+            return 1;
+        }
+        if (firstP.yearOfDeath = secondP.yearOfDeath) {
+            return 0;
+        }
+    }
+    )
+
+    return oldest[0].name
 
 };
 
